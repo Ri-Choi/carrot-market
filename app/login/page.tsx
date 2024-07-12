1f67909a019e71dd7c63642646d4892e28f4d4ab
Ri-Choi <chlfl2121@gmail.com>
2024-07-12 15:41:19 +0900

    Authentication UI

---
+++ app/login/page.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import FormButton from "@/components/form-btn";
+import FormInput from "@/components/form-input";
+import SocialLogin from "@/components/social-login";
+
+const LogIn = () => {
+  return (
+    <div className="flex flex-col gap-10 py-8 px-6">
+      <div className="flex flex-col gap-2 *:font-medium">
+        <h1 className="text-2xl">안녕하세요!</h1>
+        <h2 className="text-xl">Log in with email and password.</h2>
+      </div>
+      <form className="flex flex-col gap-3">
+        <FormInput type="email" placeholder="Email" required errors={[]} />
+        <FormInput
+          type="password"
+          placeholder="Password"
+          required
+          errors={[]}
+        />
+        <FormButton loading={false} text="Create Account" />
+      </form>
+      <SocialLogin />
+    </div>
+  );
+};
+
+export default LogIn;
